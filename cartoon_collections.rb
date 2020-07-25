bae931ac771998170aabcf1f3113d89786cf1cee
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-25 00:19:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,1 +1,17 @@
+def square_array(array)
+  array.map { |elem| elem * elem }
+end
 
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
+end
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
+end
+
+def find_valid_calls(planeteer_calls)
+  valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+
+  planeteer_calls.find {|call| valid_calls.include?(call)}
+end
